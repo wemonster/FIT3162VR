3a2422fb47b444c8841ed728bb2bc6fc547fc4cc
wemonster <peteryang970922@gmail.com>
2019-08-08 12:55:01 +1000

    Asset folder

---
+++ Unity/Unity3DGlobe/Scripts/DataLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataLoader : MonoBehaviour {
+    public DataVisualizer Visualizer;
+	// Use this for initialization
+	void Start () {
+        TextAsset jsonData = Resources.Load<TextAsset>("loc_data_large");
+        string json = jsonData.text;
+        SeriesArray data = JsonUtility.FromJson<SeriesArray>(json);
+        
+        Visualizer.CreateMeshes(data.AllData);
+
+    }
+	
+	void Update () {
+	
+	}
+}
+[System.Serializable]
+public class SeriesArray
+{
+    public SeriesData[] AllData;
+}
