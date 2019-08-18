7621c71f0aab6d9704c2f26d236e60f3a8bcb212
wemonster <peteryang970922@gmail.com>
2019-08-18 10:20:07 +1000

    show information

---
+++ Unity/Unity3DGlobe/Scripts/showArcInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showArcInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    public void showInfo(string oslic,string dslic,float volume)
+    {
+        Text origin = transform.Find("Origin").GetComponent<Text>();
+        Debug.Log(origin);
+        origin.text = oslic;
+
+        Text destination = transform.Find("Destination").GetComponent<Text>();
+        destination.text = dslic;
+
+        Text vol = transform.Find("Volume").GetComponent<Text>();
+        string txt = string.Format("{0}", volume);
+        vol.text = txt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity/Unity3DGlobe/Scripts/showLocationInfo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showLocationInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int children = transform.childCount;
+        string[] attributes = { "SLIC", "CC", "SORT", "CAP", "SPAN", "START" };
+        Text SLIC = transform.Find("SLIC").GetComponent<Text>();
+        SLIC.text = "89";
+        SLIC.color = Color.white;
+        //SLIC.transform.position = new Vector3(0, 0, 0);
+
+        Text CC = transform.Find("CC").GetComponent<Text>();
+        CC.text = "PR";
+
+        Text SORT = transform.Find("SORT").GetComponent<Text>();
+        SORT.text = "P";
+
+        Text CAP = transform.Find("CAP").GetComponent<Text>();
+        CAP.text = "69";
+
+        Text SPAN = transform.Find("SPAN").GetComponent<Text>();
+        SPAN.text = "403";
+
+        Text START = transform.Find("START").GetComponent<Text>();
+        START.text = "400";
+        //foreach (var item in attributes)
+        //{
+        //    Text txt = transform.Find("Text").GetComponent<Text>();
+        //}
+        //Text[] txts = new Text[children];
+
+        //for (int i = 0; i < children; i++)
+        //{
+        //    Text txt = (Text)transform.GetChild(i);
+        //    Debug.Log(transform.GetChild(i));
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
